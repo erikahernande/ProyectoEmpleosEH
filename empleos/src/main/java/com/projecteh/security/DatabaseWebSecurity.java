252126284d5624a2a122573f359522fd74738f65
erikahernande <erika.hernandezmay@cbtis72.edu.mx>
2022-10-08 18:03:44 -0500

    Empleos App hasta el video 9 de la leccion 13

---
+++ empleos/src/main/java/com/projecteh/security/DatabaseWebSecurity.java
@@ -0,0 +1,26 @@
+package com.projecteh.security;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class DatabaseWebSecurity extends WebSecurityConfigurerAdapter {
+	@Autowired
+	private DataSource dataSource;
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication().dataSource(dataSource)
+				.usersByUsernameQuery("select username, password, estatus from usuarios where username=?")
+				.authoritiesByUsernameQuery("select u.username, p.perfil from usuarioPerfil up "
+						+ "inner join usuarios u on u.id = up.idUsuario "
+						+ "inner join perfiles p on p.id = up.idPerfil " + 
+						"where u.username = ?");
+	}
+}
